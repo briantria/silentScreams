cf8ce0dfd501eb12924760e768e8e63a553d5d4e
Brian Tria <btria959@gmail.com>
2015-08-09 11:15:59 +0800

    project setup

---
+++ Assets/Scripts/ScaleSprite.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Brian Tria
+ * Aug 09, 2015
+ * 
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class ScaleSprite : MonoBehaviour 
+{
+	[SerializeField] private SpriteRenderer m_spriteToScale;
+
+	protected void Awake ()
+	{
+		if(m_spriteToScale == null)
+		{
+			Debug.LogError("ScaleSprite: No reference to m_spriteToScale!");
+		}
+	}
+
+	protected void Start () 
+	{
+		FitToScreen();
+	}
+
+	private void FitToScreen ()
+	{
+		Vector2 spriteScale = Vector2.one;
+		
+		float spriteHeight = m_spriteToScale.bounds.size.y;
+		float spriteWidth  = m_spriteToScale.bounds.size.x;
+
+		float worldScreenHeight = Camera.main.orthographicSize * 2;
+		float wolddScreenWidth  = worldScreenHeight / Screen.height * Screen.width;
+
+		spriteScale.y = worldScreenHeight / spriteHeight;
+		spriteScale.x = wolddScreenWidth  / spriteWidth;
+
+		transform.localScale = spriteScale;
+	}
+}
